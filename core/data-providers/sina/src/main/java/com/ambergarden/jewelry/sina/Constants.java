e913c0ed82889394f8d31fe11f83db384acc29a3
hanyuzhang <silverfox715@sina.com>
2015-09-20 14:12:49 +0800

    Add sina data provider and add provider's rest stub project In this change, we have add sina data provider project and its dependency provider rest stub project. The data provider rest stub project is used to hold all DTOs for data providers. File base.xsd is used to define all basic DTO types, e.g. Descriptive entity which has a name and description. File stock.xsd is used to record all stock relative DTO types. All these xsd files will be converted into java classes. In sina provider project, we have add a StockListProvider which will help to retrieve all stocks from sina. It uses a HttpClient to send a request to sina, and uses Jackson's ObjectMapper to convert the retrieved JSON into our DTO. Since the JSON retrieved from sina is not standard(field names are not double quoted), we have configured the ObjectMapper to allow un-quoted field names. And since sina has provide more properties than we have expected for listing stocks, and does not have a corresponding id, we have configured our ObjectMapper to not fail with unknown properties.
    Next steps:
    Create test project which contains all integration tests. We need to write integration tests for testing sina data provider.
    Create our exception system, which could be help for our logging system

---
+++ core/data-providers/sina/src/main/java/com/ambergarden/jewelry/sina/Constants.java
@@ -0,0 +1,8 @@
+package com.ambergarden.jewelry.sina;
+
+public class Constants {
+   public static final String SHANGHAI_STOCK_LIST_URL_FORMAT
+      = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=%d&num=500&node=sh_a";
+   public static final String SHENZHEN_STOCK_LIST_URL_FORMAT
+      = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=%d&num=500&node=sz_a";
+}
---
+++ core/data-providers/sina/src/main/java/com/ambergarden/jewelry/sina/provider/StockListProvider.java
@@ -0,0 +1,106 @@
+package com.ambergarden.jewelry.sina.provider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.ambergarden.jewelry.schema.beans.stock.Stock;
+import com.ambergarden.jewelry.schema.beans.stock.StockCategory;
+import com.ambergarden.jewelry.sina.Constants;
+import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+/**
+ * Class used to retrieve all stocks by category
+ */
+public class StockListProvider {
+   /**
+    * List all stocks under a specific category
+    * @return all stocks under a specific category
+    */
+   public List<Stock> listAllStocks(StockCategory category) {
+      List<Stock> result = new ArrayList<Stock>();
+
+      int pageNum = 1;
+      boolean complete = false;
+      while (!complete) {
+         String stockListString = retrieveStocks(pageNum, category);
+         List<Stock> stockList = parseToDTO(stockListString);
+         if (stockList != null && stockList.size() > 0) {
+            result.addAll(stockList);
+         } else {
+            complete = true;
+         }
+         pageNum++;
+      }
+      return result;
+   }
+
+   private String retrieveStocks(int pageNum, StockCategory category) {
+      HttpClient client = HttpClientBuilder.create().build();
+      String url = constructRequestURL(pageNum, category);
+      HttpResponse response = null;
+      try {
+         response = client.execute(new HttpGet(url));
+      } catch (IOException e) {
+         // TODO: Throw an exception when we have built the exception system
+      }
+
+      if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+         // TODO: Throw an exception to indicate that we've failed to retrieve stocks
+      }
+
+      StringBuffer result = new StringBuffer();
+      try {
+         InputStream contentStream = response.getEntity().getContent();
+         BufferedReader rd = new BufferedReader(
+               new InputStreamReader(contentStream, "GBK"));
+
+         String line = "";
+         while ((line = rd.readLine()) != null) {
+            result.append(line);
+         }
+      } catch (IOException e) {
+         // TODO: Throw an exception to indicate that we've failed to read the content
+      }
+      return result.toString();
+   }
+
+   private List<Stock> parseToDTO(String stockListString) {
+      List<Stock> result = new ArrayList<Stock>();
+      try {
+         ObjectMapper mapper = new ObjectMapper();
+         CollectionType arrayType = mapper.getTypeFactory().constructCollectionType(
+               List.class, Stock.class);
+         mapper.getFactory().configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+         result = mapper.readValue(stockListString, arrayType);
+      } catch (Exception ex) {
+         // TODO: Throw an exception to indicate that we've failed to read the content
+      }
+      return result;
+   }
+
+   private String constructRequestURL(int pageNum, StockCategory category) {
+      switch(category) {
+      case SHANGHAI:
+         return String.format(Constants.SHANGHAI_STOCK_LIST_URL_FORMAT, pageNum);
+      case SHENZHEN:
+         return String.format(Constants.SHENZHEN_STOCK_LIST_URL_FORMAT, pageNum);
+      default:
+         // TODO: Thrown an exception when we have built the exception system
+         return "";
+      }
+   }
+}
