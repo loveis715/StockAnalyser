badc26731460f6c1cba7554052ce35eb851c588f
hanyuzhang <silverfox715@sina.com>
2015-09-21 16:30:59 +0800

    Initial commit for jewelry's UI This change submits the initial UI for jewelry. The steps for creating that UI are: 1. Use Git Bash to navigate to the destination folder which contains the UI Project 2. Execute command sencha -sdk <ROOT_FOLDER>/build/libs/extjs-XXX/ generate app -name jewelry -path src/main/sencha -th ext-theme-crisp.    Here, the <ROOT_FOLDER> is the root of our repository, and we have put ExtJS library at folder <ROOT_FOLDER>/build/libs/extjs-XXX, and we have used crisp theme for jewelry's UI. 3. Change directory to src/main/sencha, and run sencha app watch. If the UI appears at http://localhost:1841/, it succeeds

---
+++ ui/src/main/sencha/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('jewelry.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'jewelry'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ ui/src/main/sencha/resources/Readme.md
@@ -0,0 +1,4 @@
+# jewelry/resources
+
+This folder contains resources (such as images) needed by the application. This file can
+be removed if not needed.
