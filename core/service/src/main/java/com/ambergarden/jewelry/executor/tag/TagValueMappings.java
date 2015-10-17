cef958cabb56bf0f0090a0fd958076317e3cf9aa
hanyuzhang <silverfox715@sina.com>
2015-10-17 10:57:21 +0800

    Add more levels for volume increment

--- core/service/src/main/java/com/ambergarden/jewelry/executor/tag/TagValueMappings.java
+++ core/service/src/main/java/com/ambergarden/jewelry/executor/tag/TagValueMappings.java
@@ -2,8 +2,8 @@ package com.ambergarden.jewelry.executor.tag;
 
 public class TagValueMappings {
    public static final double[] VOLUME_TAG_LEVELS
-      = new double[] {1.3, 1.42, 1.52, 1.61, 1.70, 1.78, 1.85, 1.91, 1.96, 2.00};
-   public static final int VOLUME_TAG_MAX_SCORE = 10;
+      = new double[] {1.3, 1.46, 1.62, 1.77, 1.92, 2.06, 2.2, 2.33, 2.46, 2.58, 2.7, 2.81, 2.92, 3.02, 3.12, 3.21, 3.3, 3.38, 3.46, 3.53};
+   public static final int VOLUME_TAG_MAX_SCORE = 20;
 
    public static final int[] MASS_VOLUME_LEVELS = new int[] {15, 20, 25, 30, 35, 40, 45, 50, 60, 70};
 }
